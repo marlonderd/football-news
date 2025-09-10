3e874382f77622da2e42ac999caf25eae28df7aa
marlonderd <marlondbatara9899@gmail.com>
2025-09-10 16:15:00 +0700

    deploy ke PWS

--- football_news/settings.py
+++ football_news/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "marlond-leanderd-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://marlond-leanderd-footballnews.pbp.cs.ui.ac.id", "http://localhost", "http://127.0.0.1"
+    "https://marlond-leanderd-footballnews.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
